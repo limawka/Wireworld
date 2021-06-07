0e9eb9793e65def4bdb15b2350dd4729011a28d9
kajamyk <73692436+kajamyk@users.noreply.github.com>
2021-06-07 22:15:08 +0200

    Update WireworldCanvas.java
    Poprawiono rysowanie początkowego widoku

--- Wireworld/src/gui/WireworldCanvas.java
+++ Wireworld/src/gui/WireworldCanvas.java
@@ -7,6 +7,8 @@ public class WireworldCanvas extends JPanel {
     protected void paintComponent(Graphics g) {
         //super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.BLACK);
+        g2d.fillRect(0, 0, 1520, 1000);
         g2d.setColor(Color.DARK_GRAY);
         for (int i = 0; i < 100; i++) {
             for (int j = 0; j < 150; j++) {
